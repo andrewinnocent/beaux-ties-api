85a3178165208f2858ec240b24d8e0fd35ecc971
andrewinnocent <innocent.andrew@gmail.com>
2018-04-18 14:03:59 -0400

    Syntax Fix
    Add `\` after token.

--- scripts/bows-carts/delete-bow-cart.sh
+++ scripts/bows-carts/delete-bow-cart.sh
@@ -4,7 +4,7 @@ curl "http://localhost:4741/bows_carts/${ID}" \
   --include \
   --request DELETE \
   --header "Content-Type: application/json" \
-  --header "Authorization: Token token=${TOKEN}"
+  --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "bows_carts": {
       "id": "'"${ID}"'"
