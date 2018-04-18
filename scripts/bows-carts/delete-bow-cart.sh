e9fd03e4bbc5d466179ddc96f8f10e6ff3fc9989
andrewinnocent <innocent.andrew@gmail.com>
2018-04-18 14:01:41 -0400

    Delete record from `bows_carts`
    Updated and tested script to delete an instance of a bow tie.

--- scripts/bows-carts/delete-bow-cart.sh
+++ scripts/bows-carts/delete-bow-cart.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-curl "http://localhost:4741/bows_carts" \
+curl "http://localhost:4741/bows_carts/${ID}" \
   --include \
   --request DELETE \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}"
+  --data '{
+    "bows_carts": {
+      "id": "'"${ID}"'"
+    }
+  }'
 
 echo
