60226dad955c54075cd22e4c4ca02657a65b0df0
andrewinnocent <innocent.andrew@gmail.com>
2018-04-19 10:23:40 -0400

    Adds Wishlist Column to Bows
    For a user to select bows to mark as wishlisted.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417220404) do
+ActiveRecord::Schema.define(version: 20180419141910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20180417220404) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "wishlist", default: false, null: false
   end
 
   create_table "bows_carts", force: :cascade do |t|
