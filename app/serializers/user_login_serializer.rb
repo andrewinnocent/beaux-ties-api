b1a5d8d60a4c102156af29c6d0a07648f010a3bb
andrewinnocent <innocent.andrew@gmail.com>
2018-04-23 11:00:50 -0400

    Fix Method Name in Serializer
    Previously was receiving 500 error for sign-in/up. Corrected `:cart_id`
    to `:cart`.

--- app/serializers/user_login_serializer.rb
+++ app/serializers/user_login_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UserLoginSerializer < ActiveModel::Serializer
-  attributes :id, :email, :token, :cart_id
+  attributes :id, :email, :token, :cart
 
   def token
     Rails.application.message_verifier(:signed_token).generate(object.token)
